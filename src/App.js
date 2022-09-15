7d29cd099f944f8507ab5f1e3a4b897990e820b0
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2022-09-15 09:33:12 -0300

    fixed folders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import { Route, Routes } from "react-router-dom";
-import countries from "./data";
-import { CountryInfo } from "./pages/CountryInfo";
-import { CountryList } from "./pages/CountryList";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-      </Routes>
     </div>
   );
 }
