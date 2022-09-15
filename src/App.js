6058d146b221476d5a4be8200625153cea737f29
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2022-09-15 10:50:11 -0300

    country list and info

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import { Comment } from "./pages/Comments/index";
 import { Form } from "./pages/Comments/form";
 import { EditPage } from "./pages/Comments/edit";
+import { CountryList } from "./pages/CountryList";
+import countries from "./data";
+import { CountryInfo } from "./pages/CountryInfo";
 
 function App() {
   return (
@@ -10,6 +13,14 @@ function App() {
         <Route path="/" element={<Comment />} />
         <Route path="/form" element={<Form />} />
         <Route path="/:id" element={<EditPage />} />
+        <Route
+          path="/country-list"
+          element={<CountryList countries={countries} />}
+        />
+        <Route
+          path="/country-list/:id"
+          element={<CountryInfo countries={countries} />}
+        />
       </Routes>
     </div>
   );
