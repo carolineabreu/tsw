96186a7c0f9d7db0452326afb1418abb5db5e8fc
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2022-09-15 00:51:05 -0300

    add images, country list and info done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import countries from "./data";
+import { CountryInfo } from "./pages/CountryInfo";
+import { CountryList } from "./pages/CountryList";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route
+          path="/country-list"
+          element={<CountryList countries={countries} />}
+        />
+        <Route
+          path="/:id"
+          element={<CountryInfo countries={countries} />}
+        />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/CountryList/index.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export function CountryList(countries) {
+  const [search, setSearch] = useState("");
+  return (
+    <div>
+      <div>
+        <input placeholder="Enter Country" onChange={(e) => setSearch(e.target.value)} />
+      </div>
+      {countries.countries.filter(countries => {
+        if (search === "") {
+          return countries;
+        } else if (countries.name.toLowerCase().includes(search.toLowerCase())) {
+          return countries;
+        }
+      }).map((currentCountry) => {
+        return (
+          <Link to={`/${currentCountry.id}`}>
+            {currentCountry.name}
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
