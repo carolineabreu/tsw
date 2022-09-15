cbfb77f75b24edf7e27f8b00750acc493f7d275e
larissambn <larissameo1@gmail.com>
2022-09-15 14:47:31 -0300

    form apagada

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import { Comment } from "./pages/Comments/index";
-import { Form } from "./pages/Comments/form";
 import { EditPage } from "./pages/Comments/edit";
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Comment />} />
-        <Route path="/form" element={<Form />} />
         <Route path="/:id" element={<EditPage />} />
       </Routes>
     </div>
