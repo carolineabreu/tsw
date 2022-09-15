543b4d6b7b51b2e7faa569650f7d47726296d77a
Zimarlen Silva <zimarlensilva@gmail.com>
2022-09-15 12:39:47 -0700

    last changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
-
-import './App.css';
-import Navbar  from "./components/Navbar/Navbar";
-import { Routes, Route } from "react-router-dom";
-import CountryList from "./Pages/CountryList";
-import SignUp from "./Pages/Sig Up/SignUp";
-import Home from "./Pages/Home/Home";
-// import SearchBar from "@mui/material/SearchBar";
-// import List from "./Components/List"
-
 import { Routes, Route } from "react-router-dom";
 import { Comment } from "./pages/Comments/index";
+import { Form } from "./pages/Comments/form";
 import { EditPage } from "./pages/Comments/edit";
 import { CountryList } from "./pages/CountryList";
 import countries from "./data";
 import { CountryInfo } from "./pages/CountryInfo";
 
-import { SignUp } from "./pages/SignUp";
-
-
 function App( ) {
   return (
     <div className="App">
-
-     <Navbar />
-     <Routes>
-     <Route path="/" element={<Home />} />
-     {/* <Route path="country-list" element={<CountryList countries={countries}  />} /> */}
-     <Route path="/sign-up" element={<SignUp />} />
-     </Routes>
+      <Routes>
+        <Route path="/" element={<Comment />} />
+        <Route path="/form" element={<Form />} />
         <Route path="/:id" element={<EditPage />} />
-        <Route path="/sign-up" element={<SignUp />} />
         <Route
           path="/country-list"
           element={<CountryList countries={countries} />}
