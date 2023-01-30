bce7698c7f34263153ffa56dea600e4717816ef6
Zimarlen Silva (Zih) <90367705+ZihSilva@users.noreply.github.com>
2023-01-29 22:09:33 -0800

    Revert "alt"

--- src/pages/AboutUs/Us.js
+++ src/pages/AboutUs/Us.js
@@ -1,32 +1,31 @@
 import AboutUs from './About';
-import './AboutUs.css';
-import image6 from "../../images/Lari.jpg"
-import image7 from "../../images/Carol.jpg"
-import image8 from "../../images/Zih.jpg"
-import { Link } from 'react-router-dom';
+import style from "./AboutUs.module.css";
+import image6 from "../../images/Lari.jpg";
+import image7 from "../../images/Carol.jpg";
+import image8 from "../../images/Zih.jpg";
 
 
 
 function Us() {
   return (
-    <div className="cards">
-    <h1 className="card-title">Meet Our Team</h1>
-    <div className="cards-containers">
-      <div className='cards-wrapper'>
-        <ul className='cards-items'>
+    <div className={style.cards}>
+      <h1 className={style.h1}>The Team</h1>
+      <div className={style.cardsContainers}>
+        <div className={style.container}>
+          <ul className={style.cardItem}>
             <AboutUs
               src={image6}
-              text="My name is Larissa, I'm 26 years old, brazilian, and I love travelling. I have lived in Ireland and Italy besides Brazil, and I plan to travel a lot more around the world."
+              text="My name is Larissa, I'm 26 years old, brazilian, and I love travelling. I have lived in Ireland and Italy besides Brazil, and I plan to travel a lot more around the world. I'm currentlly studying web-development  at Ironhack bootcamp, and here's my Linkedin profile to connect: https://www.linkedin.com/in/larissa-bueno-a7a828214"
               path='about-us'
             />
             <AboutUs
               src={image7}
-              text="Hi, my name is Caroline, I'm from Brazil and love to know different places and cultures. I made a trip in 2019 to Europe and I had a chance to visit many places I had had always wanted to see. While studying web development at Ironhack, I'm already dreaming about places I want to go next."
+              text="Hi, my name is Caroline, I'm from Brazil and love to know different places and cultures. I made a trip in 2019 to Europe and I had a chance to visit many places I had had always wanted to see. While studying web development at Ironhack, I'm already dreaming about places I want to go next. Please check my linkedin profile to connect :https://www.linkedin.com/in/caroline-santos-abreu/ "
               path='about-us'
             />
             <AboutUs
               src={image8}
-              text="Hi, I’m Zih, a Brazilian native, who loves to travel and discover the world. Although I enjoy traveling on my own, safety has always been a huge concern for me. This website was created to help women find safe destinations, and to show them that travelling can be safe."
+              text="Hi, I'm Zih, a Brazilian native who loves to travel and discover the world. Although I enjoy traveling solo, safety has always been a concern. The truth is that women face significant obstacles, especially when traveling alone. We created this website to help women find safe destinations and show that traveling can be safe and full of positive and rewarding experiences if planned well. If would like to connect: https://www.linkedin.com/in/zimarlen-silva-49a7a0238/"
               path='about-us'
             />
           </ul>
