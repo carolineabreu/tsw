74fceaa2b5c5fc5e813957979324c9fb5ce3d564
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2023-02-22 14:33:12 -0300

    :recycle: refactor: remove unsued imports, add toaster and change signup import and pathname

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
+import { Toaster } from "react-hot-toast";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import 'antd/dist/antd.min.css';
 import Navbar from "./components/Navbar/Navbar";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-// import { EditPage } from "./pages/Comments/edit";
-import { CountryList } from "./pages/CountryList/CountryList";
 import countries from "./data";
-import { CountryInfo } from "./pages/CountryInfo/CountryInfo";
-import { SignUp } from "./pages/SignUp";
-import { Comment } from "./pages/Comment/Comment";
 import Us from "./pages/AboutUs/Us";
+import { Comment } from "./pages/Comment/Comment";
+import { CountryInfo } from "./pages/CountryInfo/CountryInfo";
+import { CountryList } from "./pages/CountryList/CountryList";
+import Home from "./pages/Home/Home";
+import { Signup } from "./pages/Signup";
+
 
 function App() {
   return (
@@ -17,9 +17,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/comments" element={<Comment />} />
-        {/* <Route path="/:id" element={<EditPage />} /> */}
-        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about-us" element={<Us />} />
 
         <Route
@@ -31,6 +30,7 @@ function App() {
           element={<CountryInfo countries={countries} />}
         />
       </Routes>
+      <Toaster />
     </div>
   );
 }
