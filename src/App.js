3ea35b7f14eea08a69e164cfd231aeb52efc61ff
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2023-02-23 18:42:34 -0300

    :sparkles: feat: add profile page and route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { AuthContextComponent } from "./contexts/authContext";
 import countries from "./data";
 import Us from "./pages/AboutUs/Us";
 import { Comment } from "./pages/Comment/Comment";
@@ -9,6 +11,7 @@ import { CountryInfo } from "./pages/CountryInfo/CountryInfo";
 import { CountryList } from "./pages/CountryList/CountryList";
 import Home from "./pages/Home/Home";
 import { Login } from "./pages/Login";
+import { Profile } from "./pages/Profile";
 import { Signup } from "./pages/Signup";
 
 
@@ -16,23 +19,26 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/comments" element={<Comment />} />
-        <Route path="/about-us" element={<Us />} />
+      <AuthContextComponent>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<ProtectedRoute component={Profile} />} />
+          <Route path="/comments" element={<Comment />} />
+          <Route path="/about-us" element={<Us />} />
 
-        <Route
-          path="/country-list"
-          element={<CountryList countries={countries} />}
-        />
-        <Route
-          path="/country-list/:id"
-          element={<CountryInfo countries={countries} />}
-        />
-      </Routes>
-      <Toaster />
+          <Route
+            path="/country-list"
+            element={<CountryList countries={countries} />}
+          />
+          <Route
+            path="/country-list/:id"
+            element={<CountryInfo countries={countries} />}
+          />
+        </Routes>
+        <Toaster />
+      </AuthContextComponent>
     </div>
   );
 }
