d01caa58bc31cdac135a831a4edc7a2aa603e0ae
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2023-03-13 11:24:11 -0300

    :recycle: refactor: change paths

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,10 +1,10 @@
-import CardItem from './CardItem';
-import "./Cards.css"
-import images from "../../images/ireland2.jpg";
-import images2 from "../../images/iceland1.jpg";
-import images3 from "../../images/sweden1.jpg";
 import images4 from "../../images/canada.jpg";
+import images2 from "../../images/iceland1.jpg";
+import images from "../../images/ireland2.jpg";
+import images3 from "../../images/sweden1.jpg";
 import images5 from "../../images/swiss.jpg";
+import CardItem from './CardItem';
+import "./Cards.css";
 
 function Cards() {
   return (
@@ -17,13 +17,13 @@ function Cards() {
               src={images}
               text='Journey along epic trails, hike to mountain peaks and discover the stunning landscapes of Ireland and visit the top 1 safest country in the world.'
               label=' 1- Ireland'
-              path='country-list/IRL'
+              path='/countries/63f3c1843561e75d78c8e496'
             />
             <CardItem
               src={images2}
               text='Iceland is a peaceful country in the Atlantic Ocean with gorgeous nature and open-minded people. But it has something more important to offer female solo travelers – safety.'
               label='2- Iceland'
-              path='country-list/ISL'
+              path='/countries/63fbabc602b9488a3c29859c'
             />
           </ul>
           <ul className='cards-items'>
@@ -31,19 +31,19 @@ function Cards() {
               src={images3}
               label='3- Sweden'
               text='Sweden is a great place for solo female travelers. It’s easy to get around, and there are lots of friendly people to meet. '
-              path='country-list/SWE'
+              path='/countries/63fe67bb5590c4a72880aff5'
             />
             <CardItem
               src={images4}
               text=" Of all the countries in the Americas, Canada is considered the safest destination for women travelers."
               label='4- Canada'
-              path='country-list/CAN'
+              path='/countries/63fe68bed6908bebd244de2e'
             />
             <CardItem
               src={images5}
               text="Switzerland is an incredibly safe country for women, and one of Europe's safest places for females to travel alone."
               label='5- Switzerland'
-              path='country-list/CHE'
+              path='/countries/63fe68ccd6908bebd244de2f'
             />
           </ul>
         </div>
